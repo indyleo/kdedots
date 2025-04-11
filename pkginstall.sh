0b6c677806f01818cbd88425eaa269f21859234d
Indy <Indy@debian.org>
2025-04-11 14:09:11 -0400

    making sure a clip thing is there

--- pkginstall.sh
+++ pkginstall.sh
@@ -40,7 +40,7 @@ apt install -y \
     firmware-misc-nonfree fonts-font-awesome fontconfig fonts-noto fonts-ubuntu fonts-jetbrains-mono extra-cmake-modules qt6-tools-dev kwin-dev \
     libkf6configwidgets-dev gettext libkf6crash-dev libkf6globalaccel-dev libkf6kio-dev libkf6service-dev libkf6notifications-dev libkf6kcmutils-dev \
     libkdecorations3-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-shm0-dev libx11-dev libxext-dev qt6-base-dev qt6-svg-dev libkf6windowsystem-dev \
-    qt6-base-dev-tools figlet xdo xdotool pulseaudio-utils pipewire-alsa pipewire-jack pipewire-audio krdc steam-devices
+    qt6-base-dev-tools figlet xdo xdotool pulseaudio-utils pipewire-alsa pipewire-jack pipewire-audio krdc steam-devices xclip
 
 echo "Packages installed successfully"
 
