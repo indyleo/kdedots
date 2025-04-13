bf729d09dadd3b290f1f965b88c06490374fbd0b
Indy <Indy@debian.org>
2025-04-12 23:36:49 -0400

    fix

--- run.sh
+++ run.sh
@@ -24,11 +24,11 @@ if [[ ! -f ./utils.sh ]]; then
     exit 1
 fi
 
-echo "Creating directories..."
-create_directories
-
 # Source the utils.sh file
 source ./utils.sh
+
+echo "Creating directories..."
+create_directories
 
 # Check if the package.conf file exists
 if [[ ! -f ./package.conf ]]; then
