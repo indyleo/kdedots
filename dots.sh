15d56c128fcc66897674ee3a088f9df4d8be86f0
Indy <Indy@debian.org>
2025-04-13 03:54:10 -0400

    dots

--- dots.sh
+++ dots.sh
@@ -21,20 +21,70 @@ else
     git clone "$REPO_URL"
 fi
 
+FILES_HOME=(
+    .zshrc
+    .zshenv
+    .zprofile
+    .profile
+    .bashrc
+    .bash_profile
+    .hooksrc
+    .aliasrc
+    .functionrc
+)
+
+DIRS_CONFIG=(
+    alacritty
+    tmux
+    nvim
+    ohmyposh
+    fastfetch
+    yazi
+    git
+)
+
+FILES_CONFIG=(
+    mimeapps.list
+    user-dirs.dirs
+    user-dirs.locale
+)
+
+for file in "${FILES_HOME[@]}"; do
+    if [ -f "$HOME/$file" ]; then
+        command rm -f "$HOME/$file"
+    fi
+done
+
+for dir in "${DIRS_CONFIG[@]}"; do
+    if [ -d "$HOME/.config/$dir" ]; then
+        command rm -rf "$HOME/.config/$dir"
+    fi
+done
+
+for file in "${FILES_CONFIG[@]}"; do
+    if [ -f "$HOME/.config/$file" ]; then
+        command rm -f "$HOME/.config/$file"
+    fi
+done
+
+if [ -d ~/.local/share/figletfonts ]; then
+    command rm -rf ~/.local/share/figletfonts
+fi
+
 # Check if the clone was successful
 if [ $? -eq 0 ]; then
     cd "$REPO_NAME"
-    stow --target="$HOME" --adopt zsh
-    stow --target="$HOME" --adopt shell
-    stow --target="$HOME" --adopt figletfonts
-    stow --target="$HOME" --adopt xdg
-    stow --target="$HOME" --adopt git
-    stow --target="$HOME" --adopt yazi
-    stow --target="$HOME" --adopt tmux
-    stow --target="$HOME" --adopt nvim
-    stow --target="$HOME" --adopt ohmyposh
-    stow --target="$HOME" --adopt alacritty
-    stow --target="$HOME" --adopt fastfetch
+    stow --target="$HOME" zsh
+    stow --target="$HOME" shell
+    stow --target="$HOME" figletfonts
+    stow --target="$HOME" xdg
+    stow --target="$HOME" git
+    stow --target="$HOME" yazi
+    stow --target="$HOME" tmux
+    stow --target="$HOME" nvim
+    stow --target="$HOME" ohmyposh
+    stow --target="$HOME" alacritty
+    stow --target="$HOME" fastfetch
     cd "$ORIGINAL_DIR"
 else
     echo "Failed to clone the repository."
