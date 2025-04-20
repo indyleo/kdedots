76ea033bbf7c1ead2f986915eee33d9fe90e0618
Indy <Indy@debian.org>
2025-04-20 17:53:32 -0400

    nord gtk theme install

--- themer.sh
+++ themer.sh
@@ -17,6 +17,15 @@ git_clone https://github.com/guillaumeboehm/Nordzy-cursors ~/Github/Nordzy-curso
 echo "Icons Theme"
 wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
 
+echo "Setting up Nord GTK Theme"
+mkdir -vp ~/.local/share/themes
+tag_nord=$(git ls-remote --tags https://github.com/EliverLara/Nordic.git | grep -o 'refs/tags/.*' | sed 's/refs\/tags\///' | grep -v '{}' | sort -V | tail -n 1)
+wget "https://github.com/EliverLara/Nordic/releases/download/${tag_nord}/Nordic.tar.xz" -O Nordic.tar.xz
+tar xf Nordic.tar.xz -C ~/.local/share/themes/
+rm -fv Nordic.tar.xz
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+
 echo "Now setting up Sddm theme"
 [[ -f ./Nordic-Plasma-6.tar.xz ]] &&  sudo tar -xf Nordic-Plasma-6.tar.xz -C /usr/share/sddm/themes/
 [[ -f ./sddm.conf ]] && sudo cp -v sddm.conf /etc/sddm.conf
