f00d7b81a000f97a8193c6317946f1eee5d0a244
Indy <Indy@debian.org>
2025-05-23 16:32:05 -0400

    themeing galor

--- themer.sh
+++ themer.sh
@@ -9,14 +9,14 @@ git_clone() {
 echo "Downloading Wallpapers..."
 git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 
-echo "Cursors Theme"
+echo "Cursor Theme's"
 mkdir -vp ~/.local/share/icons
-tag_nordzy=$(git ls-remote --tags https://github.com/guillaumeboehm/Nordzy-cursors.git | grep -o 'refs/tags/.*' | sed 's/refs\/tags\///' | grep -v '{}' | sort -V | tail -n 1)
-wget https://github.com/guillaumeboehm/Nordzy-cursors/releases/download/${tag_nordzy}/Nordzy-cursors.tar.gz
-tar xf Nordzy-cursors.tar.gz -C ~/.local/share/icons
-rm -fv Nordzy-cursors.tar.gz
+namer_capitaine=$(curl -s https://api.github.com/repos/sainnhe/capitaine-cursors/releases/latest | jq -r .name)
+wget "https://github.com/sainnhe/capitaine-cursors/releases/download/${namer_capitaine}/Linux.zip" -O Cursors.zip
+unzip Cursors.zip -d ~/.local/share/icons
+rm -fv Cursors.zip
 
-echo "Icons Theme"
+echo "Nord Icon Theme"
 wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
 
 echo "Setting up Nord GTK Theme"
@@ -25,15 +25,29 @@ tag_nord=$(git ls-remote --tags https://github.com/EliverLara/Nordic.git | grep 
 wget "https://github.com/EliverLara/Nordic/releases/download/${tag_nord}/Nordic.tar.xz" -O Nordic.tar.xz
 tar xf Nordic.tar.xz -C ~/.local/share/themes/
 rm -fv Nordic.tar.xz
-gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
-gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 
 echo "Setting up Nord Kvantum Theme"
 sudo mkdir -vp /usr/share/Kvantum
-git clone https://github.com/tonyfettes/materia-nord-kvantum.git
+git_clone https://github.com/tonyfettes/materia-nord-kvantum.git materia-nord-kvantum
 cd materia-nord-kvantum
 sudo cp -vr Kvantum/MateriaNordDark /usr/share/Kvantum
 cd "$ORIGINAL_DIR"
 rm -rfv materia-nord-kvantum
+
+echo "Gruvbox GTK Theme"
+git_clone https://github.com/TheGreatMcPain/gruvbox-material-gtk ~/Github/gruvbox-material-gtk
+cd ~/Github/gruvbox-material-gtk
+cp ./themes/* ~/.local/share/themes
+cp ./icons/* ~/.local/share/icons
+cd "$ORIGINAL_DIR"
+rm -rfv ~/Github/gruvbox-material-gtk
+
+echo "Gruvbox Kvantum Theme"
+git_clone https://github.com/sachnr/gruvbox-kvantum-themes.git ~/Github/gruvbox-kvantum-themes
+cd ~/Github/gruvbox-kvantum-themes
+sudo cp -vr ./Gruvbox-Dark-* /usr/share/Kvantum
+sudo cp -vr ./Gruvbox_Light_* /usr/share/Kvantum
+cd "$ORIGINAL_DIR"
+rm -rfv ~/Github/gruvbox-kvantum-themes
 
 echo "Done"
