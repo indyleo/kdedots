444ac4c3f528d8c3b276f33b65c15d5cf1676374
Indy <Indy@debian.org>
2025-05-24 20:41:20 -0400

    no more special pompt

--- compile.sh
+++ compile.sh
@@ -56,9 +56,6 @@ cargo install --locked yazi-fm yazi-cli
 echo "Installing neovide..."
 cargo install --git https://github.com/neovide/neovide
 
-echo "Installing oh-my-posh..."
-curl -s https://ohmyposh.dev/install.sh | bash -s
-
 echo "Installing espanso..."
 tag_esp=$(git ls-remote --tags https://github.com/espanso/espanso.git | grep -o 'refs/tags/.*' | sed 's/refs\/tags\///' | grep -v '{}' | sort -V | tail -n 1)
 wget https://github.com/espanso/espanso/releases/download/${tag_esp}/espanso-debian-x11-amd64.deb -O espanso.deb
