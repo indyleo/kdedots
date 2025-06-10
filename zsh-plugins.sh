f37657fe1206c6efe0e369f669e900ebea0c5da7
Indy <Indy@debian.org>
2025-06-10 19:38:09 -0400

    zsh plugin

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -6,10 +6,12 @@ git_clone() {
     [[ -d "$dest" ]] || git clone --depth=1 "$repo" "$dest"
 }
 
+PLUGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zplugins"
+
 echo "Cloning zsh plugins"
-git_clone https://github.com/zsh-users/zsh-history-substring-search.git ~/Zsh-Plugins/zsh-history-substring-search
-git_clone https://github.com/zsh-users/zsh-completions.git ~/Zsh-Plugins/zsh-completions
-git_clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/Zsh-Plugins/zsh-you-should-use
-git_clone https://github.com/hlissner/zsh-autopair.git ~/Zsh-Plugins/zsh-autopair
+git_clone https://github.com/zsh-users/zsh-history-substring-search.git "${PLUGINS_DIR}"/zsh-history-substring-search
+git_clone https://github.com/zsh-users/zsh-completions.git "${PLUGINS_DIR}"/zsh-completions
+git_clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${PLUGINS_DIR}"/zsh-you-should-use
+git_clone https://github.com/hlissner/zsh-autopair.git "${PLUGINS_DIR}"/zsh-autopair
 
 echo "Done"
